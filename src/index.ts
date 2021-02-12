564877fb0f247f96d1050b284dbde54f34a98121
Marlon Lückert <m.lueckert@me.com>
2021-02-12 16:29:00 +0100

    documentation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import { NextFunction, Request, Response } from 'express'
 import queue, { Options, QueueWorker, QueueWorkerCallback } from 'queue'
 import { endMiddleware } from './end-middleware'
 
+/**
+ * Data that is needed for the job in the queue
+ */
 interface JobData {
   res: Response,
   next: NextFunction
 }
+
+/**
+ * This method creates the job for the queue.
+ * 
+ * @param data The Express Response and Next object
+ */
 
 function createExpressJob(data: JobData): QueueWorker {
   return (callback?: QueueWorkerCallback) => {
@@ -23,9 +32,19 @@ function createExpressJob(data: JobData): QueueWorker {
 
 export const expressQueueMiddleware = function (config?: Options) {
 
+  /**
+   * Initialize new queue and enable autostart
+   */
   const q = queue(config);
   q.autostart = true
 
+  /**
+   * This middleware takes the request data and adds them to the queue
+   * 
+   * @param req Express Request
+   * @param res Express Response
+   * @param next Express Next Function
+   */
   const queueMiddleware = function (req: Request, res: Response, next: NextFunction) {
     
     const data = { res: res, next: next };
@@ -34,14 +53,23 @@ export const expressQueueMiddleware = function (config?: Options) {
   
   };
 
-  // merge `end` and `queue` middlewares
+  /**
+   * Merge End Middleware and Queue Middleware.
+   * The End Middleware is needed to notify the queue when a job is done
+   * 
+   * @param req Express Request
+   * @param res Express Response
+   * @param next Express Next Function
+   */
   const middleware = (req: Request, res: Response, next: NextFunction) => {
-    endMiddleware(req, res, () => {     // Inject res.end() handler to emit 'end' event
-      queueMiddleware(req, res, next); // Use this middleware
+    endMiddleware(req, res, () => {
+      queueMiddleware(req, res, next);
     });
   };
 
-  // expose queue
+  /**
+   * Expose queue object to the outside
+   */
   middleware.queue = q
 
   return middleware;
